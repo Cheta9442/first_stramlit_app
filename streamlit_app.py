85025059a2070ab56b80727ff087f7ec9f7642d2
Chetan Pardhi <chetanpardhi71@gmail.com>
2023-08-14 17:26:19 +0530

    Changes header menu and added one more item with emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@ import streamlit;
 
 streamlit.title("My Parents New Healthy Dinner");
 
-streamlit.header("Breakfast Menu");
+streamlit.header("Breakfast Favorites");
 
-streamlit.text("Omega 3 & Blueberry oatmeal");
-streamlit.text("kale, Spinach & Rocket Smoothie");
-streamlit.text("Hard-Boiled Free Range Egg");
+streamlit.text("🥣 Omega 3 & Blueberry oatmeal");
+streamlit.text("🥗 kale, Spinach & Rocket Smoothie");
+streamlit.text("🍞 Hard-Boiled Free Range Egg");
+streamlit.text("🐔 Avocado Toast");
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
