d404e909f85502ca9b68da599723d3803106aabb
Chetan Pardhi <chetanpardhi71@gmail.com>
2023-08-14 17:31:16 +0530

    Imported pandas and loaded s3 csv list in dataframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit;
+import pandas;
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 streamlit.title("My Parents New Healthy Dinner");
 
 streamlit.header("Breakfast Favorites");
@@ -10,4 +12,6 @@ streamlit.text("🍞 Hard-Boiled Free Range Egg");
 streamlit.text("🐔 Avocado Toast");
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+
+streamlit.dataframe(my_fruit_list);
