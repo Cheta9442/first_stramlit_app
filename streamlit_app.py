963e2933416cd3bd4587597ca09fa1e3ed28711b
Chetan Pardhi <chetanpardhi71@gmail.com>
2023-08-14 17:35:39 +0530

    Added multiselect to select fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,7 @@ streamlit.text("🐔 Avocado Toast");
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index));
+
 streamlit.dataframe(my_fruit_list);
